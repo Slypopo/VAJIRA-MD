54577cf8f0b99bf5d9e9f881e1d5aa5da6072367
Slypopo <anaaba059@gmail.com>
2024-06-15 23:36:05 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,24 +5,24 @@ function convertToBool(text, fault = 'true') {
 }
 
 
-global.thumb = fs.readFileSync('./lib/bugs/venom.jpg')
-const BOTNAME = 'VAJIRA-MD|-WA-BOT™';
-const FOOTERNAME = '> ＶＡＪＩＲＡ -  ＭＤ - Ｖ4';
-global.owner = process.env.OWNER_NUMBER  || '94701992015' ;  // ADD YOUR NUMBER WITHOUT +
+global.thumb = fs.readFileSync('https://telegra.ph/file/23998e60fad313cd316fc.jpg')
+const BOTNAME = 'ODS TECHNOLOGIES™';
+const FOOTERNAME = '> ODS TECHNOLOGIES V4';
+global.owner = process.env.OWNER_NUMBER  || '233596780685' ;  // ADD YOUR NUMBER WITHOUT +
     
     
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || 'youre session id' ,
+SESSION_ID: process.env.SESSION_ID || 'VAJIRA-MD=iR10TbwB#kXj2zyF-o8yx-7pC_VPXu5q2hpnhcjJelbzluXcjV1M' ,
 travaSend: process.env.TRAVA_SEND || '25' ,    
-MAX_SIZE: process.env.MAX_SIZE === undefined ? '1536': process.env.MAX_SIZE,/*add this in megabytes*/    
-OWNER_NAME: process.env.OWNER_NAME || 'VAJIRA' ,
+MAX_SIZE: process.env.MAX_SIZE === undefined ? '7000': process.env.MAX_SIZE,/*add this in megabytes*/    
+OWNER_NAME: process.env.OWNER_NAME || 'ODS-TECHNOLOGIES' ,
 COMMAND_TYPE: process.env.COMMAND_TYPE || 'button' ,
-LOGO: process.env.LOGO || `https://telegra.ph/file/ab0ea6ad90e2eb98090d9.jpg` ,
+LOGO: process.env.LOGO || `https://telegra.ph/file/23998e60fad313cd316fc.jpg` ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech',
 MONGODB_URI :process.env.MONGODB_URI === undefined ? 'mongodb+srv://sachintharashan26:9hVStJbuE3uxwRtZ@cluster0.o0ec025.mongodb.net/' : process.env.MONGODB_URI,  
-FOOTER: process.env.FOOTER || '‌👨‍💻 ＶＡＪＩＲＡ -  ＭＤ - Ｖ5 👨‍💻',    
+FOOTER: process.env.FOOTER || '‌👨‍💻 ODS - TECHNOLOGIES - Ｖ5 👨‍💻',    
 PREFIX: process.env.PREFIX || '.' ,
-LANG:  process.env.LANG || 'SI' ,    
+LANG:  process.env.LANG || 'EN' ,    
 ANTI_BAD: process.env.ANTI_BAD || false  ,
 AUTO_REACT:  process.env.AUTO_REACT  || false  ,    
 AUTO_TYPING:  process.env.AUTO_TYPING  || false  ,
